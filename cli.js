0cd29bb57d46c957aa8445fb4623df50eb2e04df
Crispin Bennett <myself@crisbennett.com>
2023-02-13 11:10:26 +1100

    Minor fixes

--- cli.js
+++ cli.js
@@ -5,16 +5,16 @@ const device_url = get_device_url();
 if (!device_url) {
   error(`You must set the env var HUAWEI_DEVICE_URL to your device url,\nincorporating your device's admin username and password.\nThe device url will look something like:\n\t "https://username:password@192.168.8.1/"`);
 }
-const command = get_command(process.argv);
 
-if (!get_command(process.argv)) {
-  error("Usage: huawei-lock lock (to band 28)ed/unlock (allow all bands) /info");
+const command = get_command(process.argv);
+if (!command) {
+  error("Usage: huawei-lock lock (to band 28) / unlock (allow all bands) / info");
 }
 
 main(command, device_url);
 
-function main(command, url) {
-  switch (command) {
+function main(cmd, url) {
+  switch (cmd) {
     case "info": commandInfo(url);
       break;
     case "lock": commandLock(url);
